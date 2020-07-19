730785b6c814d02a6d02fcb9ee51513683066d77
peter de heij <peter@dehey.nl>
2020-07-19 13:45:39 +0200

    Tussen verssie

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,17 @@
 
 // Pin defenities
 
-const int ServoPin = PB0;   // Servo Pin
+const int ServoPin = PB4;   // Servo Pin
 const int DKRechts = PB1;  // Drukknop Rechts
 const int DKLinks = PB2;   // Drukknop Links
 const int StatusLed = PB3;  // Status Led
-const int RelConf = PB4;   // Relais of configuratie
+const int RelConf = PB0;   // Relais of configuratie
 
 /* 
    EEPROM GEBRUIK
 
    1    DKMode   
-        1 = Drukknoppen links en rechts
+        1 = Drukknoppen links en rechts *
         2 = Schakelaar op drukknop Rechts
 
    2    EindLinks
@@ -28,15 +28,15 @@ const int RelConf = PB4;   // Relais of configuratie
         0-180 voor eindstand servo Rechts
 
    4    Snelheid
-        0-255 voor snelheid in stappen van 500 ms
+        1-255 voor snelheid in stappen van 5 ms
 
    5    RelMod
-        1 = Schakelt om bij servo tussen 2 stappen
-        2 = knippert bij servo links
-        3 = knippert bij servo rechts
+        1 = Schakelt om bij servo tussen 2 stappen *
+        2 = knippert bij servo links 
+        3 = knippert bij servo rechts 
    
    6    RelKnip
-        0-255 voor snelheid knipperen relais in stapjes van 100 ms
+        0-255 voor snelheid knipperen relais in stapjes van 5 ms *
 
    7    Voorekeur
         1 = Links voorkeur opstarten
@@ -44,84 +44,38 @@ const int RelConf = PB4;   // Relais of configuratie
         
 */
 int DKMode = 1;       //DK Mode 
-int EindLinks = 10;  // eind stand servo links
-int EindRechts = 140;  // eind stand servo rechts
-int Snelheid = 10;  // snelheid servo
-int RelMod = 1;  // Relais mode 
-int RelKnip = 2; // Relais knipper mode
+int EindLinks = 140;  // eind stand servo links
+int EindRechts = 10;  // eind stand servo rechts
+int Snelheid = 1*5;  // snelheid servo
+int RelMod = 3;  // Relais mode 
+int RelKnip = 100*5; // Relais knipper snelhied
 int VoorKeur = 1; // Voorkeur opstart stand
 
+
 // Variable
-int Hoek = EindRechts - EindLinks; //Hoek verstelling
-int RelaisOm = Hoek/2;
+int Mode = 1; // Welke mode zitten we
+int MomLinks = 0; // Marker links ingedrukt
+int MomRechts = 0; // Marker rechts ingedrukt
+int ContLinks = 0;
+int ContRechts = 0;
+int Stel; // Servo positie om te settten
+int ledState = LOW;
+int PosServo; // Actuele servo positie 0 = Rechts 1 = Links
+int RelStatus = 0;
+
+
+unsigned long ServoMillis = 0; // voor servo
+unsigned long RelaisMillis = 0; // voor knipperen relais
+unsigned long currentMillis = millis();
+
+
 Adafruit_SoftServo Servo;
 
-void ServoLinks() // Servo naar links met ingestelde waarde
-{
-   int Stel = EindRechts; // Begin stand
-
-      // Zet servo naar links en halverwegen het relais 
-     
-     for (int i = 0  ; i < Hoek; i++ )
-     {
-         digitalWrite(StatusLed,LOW);      
-         Servo.write(Stel);
-         delay(Snelheid);
-         Stel=Stel-1;
-         if (i == RelaisOm)
-          {
-           digitalWrite(RelConf, LOW);
-          } 
-         digitalWrite(StatusLed,HIGH);
-         delay(Snelheid);   
-     }       
-}
-
-void ServoRechts() // Servo naar Rechts met ingestelde waarde
-{
-       int Stel = EindLinks; // Begin stand
-
-      // Zet Servo naar Rechts en halverwegen het relais
-
-     for (int i = 0 ; i < Hoek ; i++ )
-     {
-        digitalWrite(StatusLed,LOW);
-        Servo.write(Stel);
-        delay(Snelheid);
-        Stel=Stel+1;
-        if (i == RelaisOm)
-         { 
-           digitalWrite(RelConf, HIGH);
-         }
-        digitalWrite(StatusLed,HIGH);
-        delay(Snelheid); 
-     }
-}
-
-void Mode1()  // Drukknoppen met relais om tijdens servo
-{
-  while(1)
-    {
-
-      if(digitalRead(DKLinks)== 0)  //Drukknop Links
-        { 
-          ServoLinks();
-        }
-
-      if(digitalRead(DKRechts)== 0) //Drukknop Rechts
-        { 
-          ServoRechts();
-       }
-
-   }
-
-}
-
-void Mode2()  // Drukknoppen met relais in knipperstand
-{
 
 
-}
+
+
+
 
 void setup() 
 {
@@ -139,35 +93,105 @@ void setup()
   // Servo op pin ServoPin
 
   Servo.attach(ServoPin);
+  Servo.write(EindLinks);
+  PosServo = 1;
+  digitalWrite(RelConf, HIGH);
   
-  if (VoorKeur == 1)
-    {
-       ServoLinks();
-    }
-    else 
-    {
-      ServoRechts(); 
-    }  
 
 }
 
 void loop() 
 {
-  
-  // Bepaal werking
-
-  if (DKMode == 1 && RelMod == 1 )
-  {
-    Mode1();
-  }
-  
+  currentMillis = millis();  // Actueel teller millis     
 
 
+  if (digitalRead(DKLinks) == 0 && MomRechts == 0 && PosServo == 0)
+    {
+       MomLinks = 1;
+       Stel = EindRechts; // Begin stand
+      
+    }
   
+  if (digitalRead(DKRechts) == 0 && MomLinks == 0 && PosServo == 1)
+    { 
+       MomRechts = 1;
+       Stel = EindLinks;
+      
+    }
+
+  // Servo stel routine  
   
+  if (currentMillis - ServoMillis >= Snelheid)
+      { 
+        ServoMillis = currentMillis;
+      
+        // Naar Links
+        if ( Stel != EindLinks && MomLinks == 1 )
+        {
+          Servo.write(Stel);
+          Stel = Stel + 1;
+          
+          // Zet Relais
+          if (RelMod == 1 && Stel == ((EindLinks-EindRechts)/2)) digitalWrite(RelConf, HIGH);
+          
+          // is de eindstand bereikt?
+          if (Stel == EindLinks)
+           {
+             MomLinks = 0;
+             PosServo = 1;
   
-  
-  
+           }
+
+        }
+
+        // Naar Rechts
+        if ( Stel != EindRechts && MomRechts == 1 )
+        {
+          Servo.write(Stel);
+          Stel = Stel - 1;
+
+          // Zet Relais
+          if (RelMod == 1 && Stel == ((EindLinks-EindRechts)/2)) digitalWrite(RelConf, LOW);
+        
+          // Is de eindstand bereikt ?          
+          if (Stel == EindRechts)
+           {
+             MomRechts = 0;
+             PosServo = 0;
+           }
+
+        }
+
+        }
+
+    //Relais knipperen in eindstand    
+    
+   if (currentMillis - RelaisMillis >= RelKnip)
+   {
+     RelaisMillis = currentMillis;
+
+    if( PosServo == 1 && RelMod == 2)
+    { 
+      if (RelStatus == LOW)
+      { 
+        RelStatus = HIGH;
+      }else{
+        RelStatus = LOW;
+      }  
+    }
+
+    if( PosServo == 0 && RelMod == 3)
+    { 
+      if (RelStatus == LOW)
+      { 
+        RelStatus = HIGH;
+      }else{
+        RelStatus = LOW;
+      }  
+    }
+
+     digitalWrite(RelConf, RelStatus);
+   }
  
 }
 
@@ -183,5 +207,6 @@ SIGNAL(TIMER0_COMPA_vect)
     counter = 0;
     Servo.refresh();
   }
+
 
 }
